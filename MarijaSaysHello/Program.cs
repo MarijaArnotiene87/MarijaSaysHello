726c46733a2ca0dd302c2166aa21fcbd8e1db5f9
MarijaArnotiene87 <marija.arnotiene@itechart-group.com>
2022-12-27 16:43:26 +0200

    Revert "Add last name"
    This reverts commit c331e142995cd11258bb3947ec51493c01fab245.

--- MarijaSaysHello/Program.cs
+++ MarijaSaysHello/Program.cs
@@ -1,7 +1,4 @@
 ﻿Console.Write("Hi! What’s your name?:  ");
 var name = Console.ReadLine();
-
-Console.WriteLine($"And what's your last name?: ");
-var lastName = Console.ReadLine();
-
-Console.WriteLine($"Nice to meet you {name} {lastName} ");
+Console.WriteLine($"Nice to meet you {name} ");
+Console.WriteLine("Marija");

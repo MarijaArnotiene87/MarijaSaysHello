f39463f2903fb0226ac9f462f9403ea1a6171f97
MarijaArnotiene87 <marija.arnotiene@itechart-group.com>
2022-12-30 13:00:31 +0200

    Exercise1 completed

--- MarijaSaysHello/Program.cs
+++ MarijaSaysHello/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Task3.TaskRunner.Task3Runner();
+            //Task3.TaskRunner.Task3Runner();
+            Task4.TaskRunner.Task4Runner();
         }
     }
 }
---
+++ MarijaSaysHello/Task4/TaskRunner.cs
@@ -0,0 +1,34 @@
+﻿namespace Marija_SaysHello.Task4
+{
+    public class TaskRunner
+    {
+        public static void Task4Runner()
+        {
+            Exercise1();
+        }
+
+        public static void Exercise1()
+        {
+            Console.Write("Hi! Please enter the positive number:  ");
+
+            var userInput = Console.ReadLine();
+
+            int userInputValue;
+            var isParsable = int.TryParse(userInput, out userInputValue);
+            if (!isParsable)
+            {
+                Console.WriteLine("Could not be parsed.");
+
+                return;
+            }
+
+            int sumOfnumbers = 0;
+            for (int i = 0; i <= userInputValue; i++)
+            {
+                sumOfnumbers = sumOfnumbers + i;
+            }
+            Console.WriteLine($"{sumOfnumbers}");
+        }
+
+    }
+}

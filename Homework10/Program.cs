51687ab7ff2949b1f27f31070921cf17acb8f280
MarijaArnotiene87 <marija.arnotiene@itechart-group.com>
2023-01-13 19:42:59 +0200

    List tasks completed

---
+++ Homework10/Program.cs
@@ -0,0 +1,25 @@
+﻿using Homework10.List;
+
+namespace Homework10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { };
+            for (int i = 1; i <= 10; i++)
+            {
+                numbers.Add(i);
+            }
+
+            TaskList.SumOfAllEvenNumbers(numbers);
+
+            List<string> words = new List<string>() { "System", "Visa", "Loading", "Optimized", "Debug" };
+
+            Console.WriteLine("Enter a length of search term :");
+            var searchLength = Console.ReadLine();
+
+            TaskList.PrintFiveLetterWord(words, int.Parse(searchLength));
+        }
+    }
+}
